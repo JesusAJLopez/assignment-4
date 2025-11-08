f02fd8e1b65be6577797af557c4aa5ccff795aae
JesusAJLopez <jajutlopez@uncg.edu>
2025-11-07 23:45:07 -0500

    Delete liontracker_project directory

--- liontracker_project/src/main/java/com/example/liontracker/repository/LionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.liontracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.liontracker.model.Lion;
-
-public interface LionRepository extends JpaRepository<Lion, Long> {
-}
